834cc217eff5237ee454fe6c0e46c428a8dfd25a
shdkej <shdkej@github.com>
2020-03-18 21:40:59 +0100

    FIX config fish link

--- config.fish
+++ config.fish
@@ -2,3 +2,15 @@ alias jl="joplin"
 alias jln="joplin mknote"
 set Theme Terlar
 set -g FZF_CTRL_T_COMMAND "command find -L \$dir -type f 2> /dev/null | sed '1d; s#^\./##'"
+set -x FZF_DEFAULT_OPS "--extended"
+set -x GOPATH "$HOME/workspace/golang"
+
+function da 
+    docker ps -a | sed 1d | fzf -1 -q "$1" | awk '{print $1}' | read -l cid
+    docker start "$cid"
+    docker exec -it "$cid" fish
+end
+
+function dr
+    docker run -it --name $1 ubuntu
+end
