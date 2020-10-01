807f1d43ce81e49defcdd7f9fbff38683a35efdc
shdkej <shdkej@gmail.com>
2020-10-01 08:25:58 +0200

    UPDATE rclone sync add .github

--- rclone.sh
+++ rclone.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 rclone sync /home/sh/wiki-blog/content onedrive:/vimwiki -v --log-file /tmp/rclone.log \
-    --config /home/sh/.config/rclone/rclone.conf --exclude ".git/**"
+    --config /home/sh/.config/rclone/rclone.conf
+    #--exclude ".git/**"
 
 rclone sync /home/sh/workspace onedrive:/workspace -v --log-file /tmp/rclone.log \
-    --config /home/sh/.config/rclone/rclone.conf --exclude "**/bin/**" \
-    --exclude ".git/**" --exclude "golang/pkg/**" --exclude "**/.terraform/**" \
+    --config /home/sh/.config/rclone/rclone.conf \
+    --exclude "**/bin/**" \
+    --exclude "golang/pkg/**" \
+    --exclude "**/.terraform/**" \
     --exclude "node_modules/**"
+    #--exclude ".git/**"
